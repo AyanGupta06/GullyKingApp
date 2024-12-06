a696bc44a165e174f2476edf915e0d2b947201fc
Ayan Gupta <160621180+AyanGupta06@users.noreply.github.com>
2024-12-06 09:54:52 -0600

    start new unranked game and enter game records

--- lib/pages/started_new_unranked_game_page.dart
+++ lib/pages/started_new_unranked_game_page.dart
@@ -3,28 +3,29 @@ import 'package:gully_king/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:gully_king/pages/new_profile_page.dart';
-import 'package:gully_king/pages/started_new_unranked_game_page.dart';
+import 'package:gully_king/pages/new_ranked_game_page.dart';
+import 'package:gully_king/pages/new_unranked_game_page.dart';
 import 'package:gully_king/pages/your_teams_page.dart';
 
 import 'home_page.dart';
 import 'new_game_page.dart';
 import 'friends_teams_page.dart';
 
-class NewUnrankedGamePage extends StatefulWidget {
-  const NewUnrankedGamePage({super.key});
+class StartedNewUnrankedGamePage extends StatefulWidget {
+  final String numberOfOvers;
+  const StartedNewUnrankedGamePage({super.key, required this.numberOfOvers});
 
   @override
-  State<NewUnrankedGamePage> createState() => _NewUnrankedGamePageState();
+  State<StartedNewUnrankedGamePage> createState() => _StartedNewUnrankedGamePageState();
 }
 
-class _NewUnrankedGamePageState extends State<NewUnrankedGamePage> {
+class _StartedNewUnrankedGamePageState extends State<StartedNewUnrankedGamePage> {
   int _selectedIndex = 0; // Default home index
-  final TextEditingController _overSelectValue = TextEditingController();
   final User? user = Auth().currentUser;
-  String? errorMessage = '';
 
   String username = "";
   String position = "";
+
 
   Future<void> _fetchUserData() async {
     if (user == null) return;
@@ -54,7 +55,7 @@ class _NewUnrankedGamePageState extends State<NewUnrankedGamePage> {
     super.initState();
     _fetchUserData();
   }
-  
+
 
   void _navigateToPage(int index) {
     setState(() {
@@ -96,75 +97,22 @@ class _NewUnrankedGamePageState extends State<NewUnrankedGamePage> {
   }
 
 
-  Widget _entryField(String hintText, TextEditingController controller, {bool isPassword = false}) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        hintText: hintText,
-        hintStyle: const TextStyle(color: Colors.blueAccent),
-        filled: true,
-        fillColor: Colors.grey[100],
-        prefixIcon: Icon(
-          isPassword ? Icons.lock : Icons.email,
-          color: Colors.blueAccent,
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0),
-          borderSide: BorderSide.none,
-        ),
-        
-      ),
-    );
-  }
-
-  Widget _errorMessage() {
-    return Text(
-      errorMessage == '' ? '' : 'Error: $errorMessage',
-      style: const TextStyle(color: Colors.white),
-      textAlign: TextAlign.left,
-    );
-  }
-
-  Widget _startButton() {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.blueAccent,
-        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-      ),
-      onPressed: _validateAndStart,
-      child: const Text("Start Game"),
-    );
-  }
-
-   void _validateAndStart () {
-    setState(() {
-      errorMessage = '';
-    });
-
-    if (_overSelectValue.text.isEmpty) {
-      setState(() {
-        errorMessage = 'Please fill this field: Number of Overs';
-      });
-      return;
-    } else {
-      Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => StartedNewUnrankedGamePage(numberOfOvers: _overSelectValue.text)),
-        );
-    }
-  }
-
-  
-
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Transform.rotate(
+          angle: 180 * 3.14159 / 180, 
+          child: Tooltip(
+            message: 'Enter Your Game Records',
+            child: IconButton(
+              icon: const Icon(Icons.sports_cricket),
+              onPressed: () {
+              },
+            ),
+          ),
+        ),
         title: const Text(
-          'Unranked Game', 
+          'Enter Your Game Records', 
           style: TextStyle(fontSize: 22, fontWeight:FontWeight.normal, color: Colors.black)
         ),
         backgroundColor: const Color.fromRGBO(53, 150, 207, 1),
@@ -180,18 +128,20 @@ class _NewUnrankedGamePageState extends State<NewUnrankedGamePage> {
         height: double.infinity,
         width: double.infinity,
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-         child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 15),
-                _entryField("Enter Number of Overs", _overSelectValue),
-                const SizedBox(height: 15),
-                _errorMessage(),
-                const SizedBox(height: 15),
-                _startButton(),
-              ],
-            ),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: [
+            //     _unrankedMatchButton(),
+            //     _unrankedMatchButton(),
+
+            // ],)
+            SizedBox(height: 20), 
+
+          ],
+        ),
       ),
       bottomNavigationBar: BottomAppBar(
         color: const Color.fromRGBO(53, 150, 207, 1),
