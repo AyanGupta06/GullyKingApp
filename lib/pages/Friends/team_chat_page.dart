568e4fadc380b844ff743a5b08683e9202d28c2f
Ayan Gupta <160621180+AyanGupta06@users.noreply.github.com>
2025-03-14 17:28:01 -0500

    now done

--- lib/pages/Friends/team_chat_page.dart
+++ lib/pages/Friends/team_chat_page.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gully_king/pages/Friends/friends_teams_page.dart';
+import 'package:gully_king/pages/New%20Game/new_game_page.dart';
+import 'package:gully_king/pages/Previous%20Games/all_previous_games_page.dart';
+import 'package:gully_king/pages/home_page.dart';
+import 'package:gully_king/pages/new_profile_page.dart';
 
 class TeamChatPage extends StatefulWidget {
   final String teamId;
@@ -17,6 +21,8 @@ class _TeamChatPageState extends State<TeamChatPage> {
   final User? user = FirebaseAuth.instance.currentUser;
   final TextEditingController _messageController = TextEditingController();
   String? userName;
+  int _selectedIndex = 3;
+
 
   @override
   void initState() {
@@ -44,6 +50,50 @@ class _TeamChatPageState extends State<TeamChatPage> {
 
     _messageController.clear();
   }
+
+  void _navigateToPage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 0: //new game
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const NewGamePage()),
+        );
+        break;
+      case 1: //old games
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const AllPreviousGamesPage()),
+        );
+        break;
+      case 2: //home
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+        break;
+      case 3: //friends
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const FriendsTeamsPage()),
+        );
+        break;
+      case 4: //profile
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const NewProfilePage()),
+        );
+        break;
+
+      default:
+      //idk
+        break;
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -161,7 +211,54 @@ class _TeamChatPageState extends State<TeamChatPage> {
             ),
           ],
         ),
-      )
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: const Color.fromRGBO(53, 150, 207, 1),
+        height: 70,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _buildBottomBarIcon(
+              icon: Icons.add_box_rounded,
+              index: 0,
+              label: 'New Game',
+            ),
+            _buildBottomBarIcon(
+              icon: Icons.file_present_sharp,
+              index: 1,
+              label: 'Records',
+            ),
+            _buildBottomBarIcon(
+              icon: Icons.home_sharp,
+              index: 2,
+              label: 'Home',
+            ),
+            _buildBottomBarIcon(
+              icon: Icons.people_alt_sharp,
+              index: 3,
+              label: 'Friends',
+            ),
+            _buildBottomBarIcon(
+              icon: Icons.person,
+              index: 4,
+              label: 'Account',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBottomBarIcon({required IconData icon, required int index, required String label}) {
+    return Tooltip(
+      message: label,
+      child: IconButton(
+        icon: Icon(icon),
+        color: _selectedIndex == index ? Colors.white : Colors.black,
+        onPressed: () => _navigateToPage(index),
+        iconSize: 30,
+      ),
     );
   }
 }
