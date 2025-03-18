857cb1e454e8e47fbdcab57a25cf0cad2a6592be
Ayan Gupta <160621180+AyanGupta06@users.noreply.github.com>
2025-03-18 09:38:46 -0500

    ii think it all works

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,216 +1,10 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:gully_king/auth.dart';
-// import 'package:gully_king/pages/Previous%20Games/all_previous_games_page.dart';
-// import 'package:gully_king/pages/new_profile_page.dart';
-// import 'package:gully_king/pages/Previous%20Games/Previous%20Unranked%20Games/previous_games_page.dart';
-// import 'New Game/new_game_page.dart'; 
-// import 'Friends/friends_teams_page.dart';
-
-// class HomePage extends StatefulWidget {
-//   @override
-//   _HomePageState createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   final User? user = Auth().currentUser;
-//   int _selectedIndex = 2; //default homeIndez
-//   String username = "";
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _fetchUserData();
-//   }
-
-//   Future<void> signOut() async {
-//     await Auth().signOut();
-//   }
-
-//   Future<void> _fetchUserData() async {
-//     // if (user == null) return;
-
-//     try {
-//       DocumentSnapshot userDoc = await FirebaseFirestore.instance
-//           .collection('users')
-//           .doc(user!.uid)
-//           .get();
-
-//       setState(() {
-//         username = userDoc['username'] ?? "N/A";
-        
-//       });
-//     } catch (e) {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(
-//           content: Text("Error fetching user data: $e"),
-//           backgroundColor: Colors.red,
-//         ),
-//       );
-//     }
-//   }
-
-
-//   Widget _userId() {
-//     return Text(
-//       user?.email ?? "User Email",
-//       style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-//     );
-//   }
-//     Widget _signOutClick() {
-//     return ElevatedButton(
-//       onPressed: signOut,
-//       child: const Text("Sign Out"),
-//     );
-//   }
-
-  
-
-//   void _navigateToPage(int index) {
-//     setState(() {
-//       _selectedIndex = index;
-//     });
-
-//     switch (index) {
-//       case 0: //new game
-//         Navigator.push(
-//           context,
-//           MaterialPageRoute(builder: (context) => const NewGamePage()),
-//         );
-//         break;
-//       case 1: //old games
-//       Navigator.push(
-//           context,
-//           MaterialPageRoute(builder: (context) => const AllPreviousGamesPage()),
-//         );
-//         break;
-//       case 2: //home
-//         break;
-//       case 3: //friends
-//       Navigator.push(
-//           context,
-//           MaterialPageRoute(builder: (context) => const FriendsTeamsPage()),
-//         );
-//         break;
-//       case 4: //profile
-//       Navigator.push(
-//           context,
-//           MaterialPageRoute(builder: (context) => const NewProfilePage()),
-//         );
-//         break;
-
-//       default:
-//       //idk
-//         break;
-//     }
-//   }
-
-  
-
-//   Widget _welcome() {
-//     return Text (
-//       "Welcome, $username",
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         leading: Transform.rotate(
-//           angle: 0, 
-//           child: Tooltip(
-//             message: 'Home',
-//             child: IconButton(
-//               icon: const Icon(Icons.home_sharp),
-//               onPressed: () {
-//               },
-//             ),
-//           ),
-//         ),
-//         title: const Text(
-//           'Home', 
-//           style: TextStyle(fontSize: 22, fontWeight:FontWeight.normal, color: Colors.black)
-//         ),
-//         backgroundColor: const Color.fromRGBO(53, 150, 207, 1),
-//         elevation: 0,
-//       ),
-//       body: Container(
-//         decoration: const BoxDecoration(
-//           image: DecorationImage(
-//             image: AssetImage('assets/bg4.png'),
-//             fit: BoxFit.cover,
-//           ),
-//         ),
-//         height: double.infinity,
-//         width: double.infinity,
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             _userId(),
-//             _welcome(),
-//             _signOutClick(),
-//           ],
-//         ),
-//       ),
-//       bottomNavigationBar: BottomAppBar(
-//         color: const Color.fromRGBO(53, 150, 207, 1),
-//         height: 70,
-//         child: Row(
-//           mainAxisSize: MainAxisSize.max,
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           children: [
-//             _buildBottomBarIcon(
-//               icon: Icons.add_box_rounded,
-//               index: 0,
-//               label: 'New Game',
-//             ),
-//             _buildBottomBarIcon(
-//               icon: Icons.file_present_sharp,
-//               index: 1,
-//               label: 'Records',
-//             ),
-//             _buildBottomBarIcon(
-//               icon: Icons.home_sharp,
-//               index: 2,
-//               label: 'Home',
-//             ),
-//             _buildBottomBarIcon(
-//               icon: Icons.people_alt_sharp,
-//               index: 3,
-//               label: 'Friends',
-//             ),
-//             _buildBottomBarIcon(
-//               icon: Icons.person,
-//               index: 4,
-//               label: 'Account',
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// //fyi tooltip is what gives the user the hint on what the button is
-//   Widget _buildBottomBarIcon({required IconData icon, required int index, required String label}) {
-//     return Tooltip(
-//       message: label,
-//       child: IconButton(
-//         icon: Icon(icon),
-//         color: _selectedIndex == index ? Colors.white : Colors.black,
-//         onPressed: () => _navigateToPage(index),
-//         iconSize: 30,
-//       ),
-//     );
-//   }
-// }
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gully_king/auth.dart';
 import 'package:gully_king/pages/Friends/chat_page.dart';
 import 'package:gully_king/pages/Friends/team_chat_page.dart';
+import 'package:gully_king/pages/LoginAndRegister/login_register_page.dart';
 import 'package:gully_king/pages/Previous%20Games/all_previous_games_page.dart';
 import 'package:gully_king/pages/new_profile_page.dart';
 import 'package:gully_king/pages/Previous%20Games/Previous%20Unranked%20Games/previous_games_page.dart';
@@ -238,6 +32,17 @@ class _HomePageState extends State<HomePage> {
     _fetchRecentContacts();
   }
 
+  // Future<void> signOut() async {
+  //   try{
+  //   await Auth().signOut();
+  //   Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+  //   } catch (e) {
+  //     print("Error logging out:  $e");
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       const SnackBar(content: Text("Error logging out"))
+  //     );
+  //   }
+  // }
   Future<void> signOut() async {
     await Auth().signOut();
   }
@@ -346,23 +151,22 @@ class _HomePageState extends State<HomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(title, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 10),
+            const Divider(),
             contact != null
-                ? Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(contact, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
                       const SizedBox(height: 10),
                       Center(
-                        child: ElevatedButton(
+                        child: 
+                        ElevatedButton.icon(
                           onPressed: onTap,
+                          icon: Icon(Icons.chat, color: Colors.white),
+                          label: Text("Chat", style: TextStyle(color: Colors.white)),
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.blue,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
                           ),
-                          child: const Text("Chat", style: TextStyle(color: Colors.white)),
                         ),
                       ),
                     ],
@@ -390,15 +194,16 @@ class _HomePageState extends State<HomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(title, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 10),
+            const Divider(),
             contact != null
-                ? Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                ? Row (
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(contact, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
                       const SizedBox(height: 10),
-                      Center(
-                        child: ElevatedButton(
+                      
+                        
+                        ElevatedButton.icon(
                           onPressed: () {
                             Navigator.push(
                               context,
@@ -407,15 +212,13 @@ class _HomePageState extends State<HomePage> {
                               ),
                             );
                           },
+                          icon: Icon(Icons.chat, color: Colors.white),
+                          label: Text("Chat", style: TextStyle(color: Colors.white)),
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.blue,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
                           ),
-                          child: const Text("Chat", style: TextStyle(color: Colors.white)),
                         ),
-                      ),
+                      
                     ],
                   )
                 : const Text("No recent contact", style: TextStyle(color: Colors.grey)),
@@ -459,17 +262,9 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            const SizedBox(height: 20),
             _welcome(),
-            // _recentlyContactedCard("Recently Contacted DMs", recentDM, () {
-            //   if (recentDM != null && recentDMName != null) {
-            //     Navigator.push(
-            //       context,
-            //       MaterialPageRoute(
-            //         builder: (context) => ChatPage(friendEmail: recentDM, friendName: recentDMName),
-            //       ),
-            //     );
-            //   }
-            // }),
+           
             _recentlyContactedCardFriend("Recently Contacted DMs", recentDM),
 
             _recentlyContactedCard("Recently Contacted Teams", recentTeam, () {
